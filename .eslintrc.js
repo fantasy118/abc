852f6f10d1fef25f4bbdd62aa889b054be7313cd
fantasy <2928361059@qq.com>
2021-07-14 17:56:55 +0800

    create project

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent:[2,4],
+    'vue/html-indent':[2,4],
+    
   }
 }
