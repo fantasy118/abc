3a942bdec17b86210f9102a5ce1f2e10463ef65a
fantasy <2928361059@qq.com>
2021-07-15 14:25:27 +0800

    Feat:add icon and antd

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/assets/iconfont/iconfont'
+
+import './icons'
+
+import { Button } from 'ant-design-vue'// svg组件
+
+// 注册到全局
+Vue.use(Button)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <svg-icon icon-class="icon-noGenerate" />
+        <svg-icon icon-class="jay-yonghuzhongxin" class-name="user-icon" />
+        <a-button type="primary">hh</a-button>
+        <!-- <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
     name: 'Home',
     components: {
-        HelloWorld
+        // HelloWorld
     }
 }
 </script>
+
+<style lang="less" scoped>
+    .user-icon{
+        color: red;
+    }
+</style>
